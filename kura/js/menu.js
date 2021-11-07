763e465d2dc269bc2555e6dcfc1802cd415bb09b
Alex222212 <93159238+Alex222212@users.noreply.github.com>
2021-11-07 18:53:23 +0300

    Add files via upload

--- kura/js/menu.js
+++ kura/js/menu.js
@@ -1,15 +1,59 @@
-const restoraunt = ('food-band')
+const cardsMenu = document.querySelector('.cards-menu')
+const sectionHeading = document.querySelector('.section-heading')
+const changeTittle = (restaurant) =>{
+    const restaurantTitle = document.querySelector('.restaurant-title')
+    const rating = document.querySelector('.rating')
+    const price = document.querySelector('.price')
+    const category = document.querySelector('.category')
+
+    restaurantTitle.textContent = restaurant.name
+    rating.textContent = restaurant.stars
+    price.textContent ='от '+restaurant.price+' pуб'
+    category.textContent= restaurant.kitchen
+    }
+
 const renderItem= (data) =>
 {
-console.log(data)
-const array = data;
-    array.forEach((element, index) => {
-        console.log(index, element)
+    data.forEach(({description, id, image, name, price}) => {
+        
+        const div = document.createElement('div')
+        div.classList.add('card')
+        div.innerHTML = `
+        <img src="${image}" alt="${name}" class="card-image" />
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title card-title-reg">${name}</h3>
+							</div>
+							<!-- /.card-heading -->
+							<div class="card-info">
+								<div class="ingredients">${description}
+								</div>
+							</div>
+							<!-- /.card-info -->
+							<div class="card-buttons">
+								<button class="button button-primary button-add-cart">
+									<span class="button-card-text">В корзину</span>
+									<span class="button-cart-svg"></span>
+								</button>
+								<strong class="card-price-bold">${price} ₽</strong>
+							</div>
+        `
+        cardsMenu.append(div)
     })}
-fetch(`./db/${restoraunt}.json`).then((responsive) => responsive.json())
-.then((data) =>{
-    renderItem(data)
+
+if (localStorage.getItem('restaurant')) {
+    const restaurant = JSON.parse(localStorage.getItem('restaurant'))
+    console.log(restaurant);
+    fetch(`./db/${restaurant.products}`).then((responsive) => responsive.json())
+    .then((data) =>{
+    renderItem(data) 
 })
-.catch((Error) =>{
-    console.log('eror')
-})
+changeTittle(restaurant)
+} else {
+    window.location.href = '/'
+}
+if (localStorage.getItem('user')) {
+}else {
+    window.location.href = '/'
+}
+
--- kura/js/restaurant.js
+++ kura/js/restaurant.js
@@ -1,12 +1,49 @@
+const cardsRestaurants = document.querySelector('.cards-restaurants')
+const authUnlock = document.querySelector('.auth_unlock')
 const renderItem = (data) =>
 {
-    console.log(data);
-    const array = data;
-    array.forEach((element, index) => {
-        console.log(index, element)
+    const BigData = data;
+    BigData.forEach((element, index) => {
+        const a = document.createElement('a');
+        const {image, kitchen, name, price, products, stars, time_of_delivery} = element
+        a.setAttribute('href', '/restaurant.html');
+        a.classList.add('card');
+        a.classList.add('card-restaurant');
+        a.dataset.products = products;
+        console.log(a);
+        a.innerHTML = `
+            <img src="${image}" alt="image" class="card-image" />
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title">${name}</h3>
+                    <span class="card-tag tag">${time_of_delivery} мин</span>
+                </div>
+                <!-- /.card-heading -->
+                <div class="card-info">
+                    <div class="rating">
+                        ${stars}
+                    </div>
+                    <div class="price">От ${price} ₽</div>
+                    <div class="category">${kitchen}</div>
+                </div>
+                <!-- /.card-info -->
+            </div>
+        `
+        a.addEventListener('click', (e) =>{
+            e.preventDefault();
+            if (localStorage.getItem('user')) {
+                localStorage.setItem('restaurant', JSON.stringify(element));
+            window.location.href = '/restaurant.html'
+            } else {
+                modalAuth.style.display = "block";
+                authUnlock.style.display = "block"
+
+            }
+        })
+        cardsRestaurants.append(a)
     });}
 
-fetch('./db/partners.json').then((responsive) => responsive.json())
+fetch('./db/partners.json').then((а) => а.json())
 .then((data) =>{
     renderItem(data)
 })
